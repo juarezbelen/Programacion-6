9e16f5887358f29fba2d5de1bdda360cbfedbcbe
belenjuarezm <beljrz@hotmail.com>
2024-06-04 11:26:47 -0300

    Agregar archivos de proyecto.

---
+++ Parcial1/Models/Empleado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parcial1.Models
+{
+    public partial class Empleado
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public DateTime? FechaIngreso { get; set; }
+        public string? Clave { get; set; }
+    }
+}
